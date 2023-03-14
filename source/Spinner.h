2140bff0c2ea83217fb154480bb98881b1c7e670
Humdinger <humdingerb@gmail.com>
2023-03-14 09:46:41 +0100

    Spinners: Add SetWithoutInvoke() to avoid an Invoke()
    We want to be able to do a SetValue() without there being an Invoke()
    triggered, which sends a message as if the user used the spinner.
    No other BControl derived class does this. This is a stop-gap until
    Haiku's BSpinner class is fixed, see [1].
    
    [1] https://dev.haiku-os.org/ticket/18305

--- source/Spinner.h
+++ source/Spinner.h
@@ -16,23 +16,36 @@
 
 class Spinner : public BSpinner {
 public:
-			Spinner(const char* name, const char* label, BMessage* message);
+						Spinner(const char* name, const char* label, BMessage* message);
 
+	// Allow not doing an Invoke() on SetValue() until Haiku's BSpinner class is fixed
+	virtual status_t	Invoke(BMessage* message = NULL);
+
+	void 	SetWithoutInvoke(int32 value);
 	void 	Increment();
 	void 	Decrement();
 	void 	SetStep(int32 step);
 
 private:
+	bool	fInvoke;
 	int32 	fStep;
 };
 
 
 class DecSpinner : public BDecimalSpinner {
 public:
-			DecSpinner(const char* name, const char* label, BMessage* message);
+						DecSpinner(const char* name, const char* label, BMessage* message);
 
+	// Allow not doing an Invoke() on SetValue() until Haiku's BSpinner class is fixed
+	virtual status_t	Invoke(BMessage* message = NULL);
+
+	void 	SetWithoutInvoke(int32 value);
+	void 	SetWithoutInvoke(double value);
 	void 	Increment();
 	void 	Decrement();
+
+private:
+	bool	fInvoke;
 };
 
 #endif // SPINNER_H
