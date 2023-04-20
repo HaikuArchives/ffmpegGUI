cd1160f32137e428d53107b929a6fd9097548fd1
Humdinger <humdingerb@gmail.com>
2023-04-20 15:16:41 +0200

    Fix showing Help file
    The previous solution would always use the first returned
    B_FIND_PATH_DOCUMENTATION_DIRECTORY, which is
    ~/config/non-packaged/documentation/packages.
    
    That's why it didn't show up when I put the stuff exactly
    there for testing...

--- source/MainWindow.cpp
+++ source/MainWindow.cpp
@@ -1851,12 +1851,17 @@ MainWindow::_OpenHelp()
 	BPathFinder pathFinder;
 	BStringList paths;
 	BPath path;
+	BEntry entry;
 
-	pathFinder.FindPaths(B_FIND_PATH_DOCUMENTATION_DIRECTORY,
+	status_t error = pathFinder.FindPaths(B_FIND_PATH_DOCUMENTATION_DIRECTORY,
 		"packages/ffmpegGUI", paths);
-	if (!paths.IsEmpty()) {
-		if (path.SetTo(paths.StringAt(0)) == B_OK) {
-			path.Append("ReadMe.html");
+
+	for (int i = 0; i < paths.CountStrings(); ++i) {
+		if (error == B_OK && path.SetTo(paths.StringAt(i)) == B_OK
+			&& path.Append("ReadMe.html") == B_OK) {
+			entry = path.Path();
+			if (!entry.Exists())
+				continue;
 			BMessage message(B_REFS_RECEIVED);
 			message.AddString("url", path.Path());
 			be_roster->Launch("text/html", &message);
